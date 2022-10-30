99463c9204aa68e52377b572e837c94b9a73a773
Carlos Dias <58646640+carlosdiasme@users.noreply.github.com>
2022-10-30 01:14:16 -0300

    Add files via upload

---
+++ utm 1.2.1/script.js
@@ -0,0 +1,40 @@
+
+function createUtm() {
+
+const urlPage = document.querySelector('#url-utm').value
+const utmSource = document.querySelector('#source-utm').value
+const utmMedium = document.querySelector('#medium-utm').value
+const utmCampaign = document.querySelector('#campaign-utm').value
+const utmTerm = document.querySelector('#term-utm').value
+const utmContent = document.querySelector('#content-utm').value
+
+if (document.querySelector('#url-utm').value != ""){
+    const utmCreated = `${urlPage}?utm_source=${utmSource}&utm_medium=${utmMedium}&utm_campaign=${utmCampaign}&utm_term=${utmTerm}&utm_content=${utmContent}`
+    document.querySelector('#result-utm').innerText = utmCreated
+}else{
+    alert("É preciso informar uma URL válida!") 
+}
+
+
+
+
+}
+
+
+function clearFields() {
+document.querySelector('#url-utm').value = "";
+document.querySelector('#source-utm').value = "";
+document.querySelector('#medium-utm').value = "";
+document.querySelector('#campaign-utm').value = "";
+document.querySelector('#term-utm').value = "";
+document.querySelector('#content-utm').value = "";
+document.querySelector('#result-utm').value = "";
+
+}
+
+
+function copyUtm() {
+const resultUtm = document.querySelector('#result-utm')
+resultUtm.select();
+document.execCommand('copy')
+}
